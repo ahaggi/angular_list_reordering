410be1f45d0a896b0448b74a767392217713e417
ahaggi <a.haggi@yahoo.com>
2019-11-03 12:59:17 +0100

    first commit - deleted unused componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { from } from 'rxjs';
@@ -12,7 +11,7 @@ import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+
   ],
   imports: [
     BrowserModule,
